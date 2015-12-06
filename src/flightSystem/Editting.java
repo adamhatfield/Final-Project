998e968dbc887fa6cfd8125da355d5b94734fe35
Adam Hatfield <adam.hatfield@me.com>
2015-12-06 00:16:08 -0500

    Updated syntax and methods

--- src/flightSystem/Editting.java
+++ src/flightSystem/Editting.java
@@ -8,45 +8,45 @@ public interface Editting {
 	
 	Scanner s = new Scanner(System.in);
 	default void removeFlight(){
-	flight.numOfFlights--;
+	Flight.numOfFlights--;
 	}
-	default void setFlightNumber(flight f,int num){
+	default void setFlightNumber(Flight f,int num){
 		f.flightNumber =num;
 	}
-		default int getFlightNumber(flight f){
+		default int getFlightNumber(Flight f){
 		return f.flightNumber;
 	}
-		default int getFlightCapacity(flight f){
+		default int getFlightCapacity(Flight f){
 		return f.flightCapacity;
 	}
-	default void setFlightCapcity(flight f,int num){
+	default void setFlightCapcity(Flight f,int num){
 		 f.flightCapacity=num;
 	}
 
-	default void setFlightCost(flight f, double num){
+	default void setFlightCost(Flight f, double num){
 		f.flightCost=num;
 	}
-	default double getFlightCost(flight f){
+	default double getFlightCost(Flight f){
 		return f.flightCost;
 	}
-	default void setFlightDuration(flight f,int hrs, int min){
+	default void setFlightDuration(Flight f,int hrs, int min){
 	//	System.out.println("You are are entering in the duration of flight "+ f.flightNumber+ ""
 	//			+ "\n Enter in hours first then mintunes.");
 //		int hrs = s.nextInt();
 //		int min= s.nextInt();
 		f.flightDuration= new Time(hrs, min, 0);
 	}
-	default Time getFlightDuration(flight f){
+	default Time getFlightDuration(Flight f){
 		return f.flightDuration;
 	}
-	default void setFlightDate(flight f,int year, int month, int day, int hr, int minute){
+	default void setFlightDate(Flight f,int year, int month, int day, int hr, int minute){
 		f.flightDate = new GregorianCalendar( year,  month,  day,  hr,  minute);
 		
 	}//You have nothing for arrival to Destination
-	default GregorianCalendar getFlightDate(flight f){
+	default GregorianCalendar getFlightDate(Flight f){
 		return f.flightDate;
 	}
-	default boolean isBooked(flight f){
+	default boolean isBooked(Flight f){
 		//return true if booked
 		boolean a;
 		if(f.flightCapacity<=f.onFlight)
@@ -55,16 +55,16 @@ public interface Editting {
 			a=false;
 		return a;
 	}
-	default void setFlightDestination (flight f, String s){
+	default void setFlightDestination (Flight f, String s){
 		f.flightDestination=s;
 	}
-	default String getFlightDestination (flight f, String s){
+	default String getFlightDestination (Flight f, String s){
 		return f.flightDestination;
 	}
-	default void setFlightStartPoint (flight f, String s){
+	default void setFlightStartPoint (Flight f, String s){
 		f.flightStartPoint=s;
 	}
-	default String getFlightStartPoint(flight f, String s){
+	default String getFlightStartPoint(Flight f, String s){
 		return f.flightStartPoint;
 	}
 
