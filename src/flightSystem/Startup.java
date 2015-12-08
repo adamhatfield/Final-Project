e0d838f7ca311c3ec32d69ac02581667f21a4de8
Adam Hatfield <adam.hatfield@me.com>
2015-12-07 19:01:34 -0500

    Created startup class. This is the main initialization class for the application. Has startup constructor that creates a window frame and creates a new gui constructor. Also has method for splashscreen, method not complete yet.

---
+++ src/flightSystem/Startup.java
@@ -0,0 +1,45 @@
+package flightSystem;
+
+import java.awt.Image;
+import java.awt.SplashScreen;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EtchedBorder;
+import javax.swing.JProgressBar;
+import javax.swing.SwingWorker;
+import java.awt.CardLayout;
+
+public class Startup extends TestGUI implements Database {
+	
+	SplashScreen screen;
+	
+	public Startup(){
+		getContentPane().setLayout(new CardLayout(0, 0));
+		JFrame frame = new TestGUI();
+		frame.setSize(500,500);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		splashScreen();
+		TestGUI main = new TestGUI();
+		
+	}
+	
+	public void splashScreen(){
+		initializeDB();
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	public static void main(String[] args){
+		Startup main = new Startup();
+	}
+}
